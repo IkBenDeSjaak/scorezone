52dd882cbd0500b27567c977a20b86017c106354
Sjaak <sjaakrest@gmail.com>
2021-11-04 14:01:18 +0100

    create login form

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,9 +1,22 @@
+import styles from './login.module.css'
 import Layout from '../../components/layout'
 
 export default function Login () {
   return (
     <>
-      <Layout><p>LOGIN</p></Layout>
+      <Layout>
+        <h2>Login</h2>
+        <p>Login with your username and password.</p>
+        <form className={styles.form} onSubmit={console.log('submit')}>
+          <label className={styles.label} htmlFor='username'>Username<input className={styles.input} id='username' type='text' /></label>
+          <label className={styles.label} htmlFor='password'>Password<input className={styles.input} id='password' type='password' /></label>
+          <label className={styles.label}><input className={`${styles.submitbutton} ${styles.input}`} id='submitbutton' type='submit' value='Login' /></label>
+          <label className={styles.labelrememberme}>
+            <input className={styles.inputrememberme} defaultChecked name='rememberme' id='rememberme' type='checkbox' />
+            Remember me
+          </label>
+        </form>
+      </Layout>
     </>
   )
 }
