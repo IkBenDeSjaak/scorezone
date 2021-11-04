76d5855b2b49812aaacdbe676cbbdcaa52b26ab2
Sjaak <sjaakrest@gmail.com>
2021-11-04 10:40:24 +0100

    create footer component

---
+++ components/footer.js
@@ -0,0 +1,22 @@
+import styles from './footer.module.css'
+
+export default function Footer () {
+  const getDateTime = () => {
+    const date = new Date()
+    const currentDate = date.toLocaleDateString()
+    const currentTime = date.toLocaleTimeString()
+    const currentDateTime = `${currentDate} ${currentTime}`
+    return currentDateTime
+  }
+
+  return (
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.content}>
+          <p>&copy; ScoreZone</p>
+          <p>{getDateTime()}</p>
+        </div>
+      </footer>
+    </>
+  )
+}
