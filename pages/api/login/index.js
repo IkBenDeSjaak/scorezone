1c9ee2820156c007da6464aa548893671343678b
Sjaak <sjaakrest@gmail.com>
2021-11-04 17:06:06 +0100

    create login route

---
+++ pages/api/login/index.js
@@ -0,0 +1,25 @@
+import { withSessionRoute } from '../../../lib/withSession'
+
+export default withSessionRoute(handler)
+
+async function handler (req, res) {
+  switch (req.method) {
+    case 'POST':
+      // const { username } = await req.body
+      // const { password } = await req.body
+      const username = 'TESTPERSOON'
+
+      // get user from database then:
+
+      const user = {
+        id: 230,
+        username: username
+      }
+
+      req.session.user = user
+
+      await req.session.save()
+
+      res.status(200).json({ name: `${username} is logged in` })
+  }
+}
