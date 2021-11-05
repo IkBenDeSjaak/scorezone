925d024f4a39b15d98e300f4be069c586312c747
Sjaak <sjaakrest@gmail.com>
2021-11-05 11:25:41 +0100

    add style to custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,13 @@
+import styles from '../styles/Custom404.module.css'
 import Layout from '../components/layout'
 
 export default function Custom404 () {
   return (
     <>
       <Layout>
-        <h1>404 - Page Not Found</h1>
+        <div className={styles.center}>
+          <h1 className={styles.message}>404 - Page Not Found</h1>
+        </div>
       </Layout>
     </>
   )
