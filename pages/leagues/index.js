1fdf65260a59e4223422ce9540418beee46a3ea6
Sjaak <sjaakrest@gmail.com>
2021-11-12 11:24:26 +0100

    add different background for each row on select leagues page

--- pages/leagues/index.js
+++ pages/leagues/index.js
@@ -11,13 +11,13 @@ export default function Leagues () {
           <h3 className={styles.fontWeightNormal}>Select all leagues from which you want to predict matches!</h3>
           <div className={styles.leaguesContainer}>
             <div className={styles.leagueRows}>
-              <div className={styles.leagueRow}>
+              <div className={`${styles.leagueRow} ${styles.leagueRowBackground1}`}>
                 <label>Eredivisie</label>
                 <div className={styles.divInput}>
                   <input type="checkbox" name="1" checked></input>
                 </div>
               </div>
-              <div className={styles.leagueRow}>
+              <div className={`${styles.leagueRow} ${styles.leagueRowBackground2}`}>
                 <label>Conference League</label>
                 <div className={styles.divInput}>
                   <input type="checkbox" name="2"></input>
