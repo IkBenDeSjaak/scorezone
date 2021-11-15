39614cec8fa31494e1ab9f585600aab32a06480f
Sjaak <sjaakrest@gmail.com>
2021-11-15 11:32:26 +0100

    implement poule settings page

--- pages/poules/[pid]/settings.js
+++ pages/poules/[pid]/settings.js
@@ -2,7 +2,10 @@ import styles from './Settings.module.css'
 
 import { withSessionSsr } from '../../../lib/withSession'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 import Layout from '../../../components/Layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 export default function Settings ({ test }) {
   const router = useRouter()
@@ -12,10 +15,113 @@ export default function Settings ({ test }) {
   return (
     <>
       <Layout>
+        <p className={styles.backButton}>
+          <Link href={`../${pid}`}>
+            <a>← Back to poule</a>
+          </Link>
+        </p>
         <h1>Settings</h1>
-        <div className={styles.buttons}>
-          <button className={styles.deleteButton}>Delete poule</button>
+        <h2>General</h2>
+        <form className={styles.form}>
+          <label className={styles.label} htmlFor='pouleName'>
+            Poule name
+            <input className={styles.input} id='pouleName' type='text' defaultValue="Jantje's poule"/>
+          </label>
+          <label className={styles.labelApprove} htmlFor="approve">
+            <input className={styles.inputapprove} name='approve' id='approve' defaultChecked={true} type='checkbox' />
+            Approve or disapprove new participants
+          </label>
+          <label className={styles.label}><input className={`${styles.submitbutton} ${styles.input}`} id='submitbutton' type='submit' value='Save settings' /></label>
+        </form>
+        <h2>Approve participants</h2>
+        {/* <p>There are no participants to approve or disapprove.</p> */}
+        <p>There are people who want to join your poule! Click on the checkmark to approve a participant for your poule or click on the cross to reject the join request.</p>
+        <div class={styles.approveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>User</th>
+                <th className={styles.approveTableName} scope='col'>Name</th>
+                <th scope='col'>Approve</th>
+                <th scope='col'>Disapprove</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Sjaakieeeeeeeee</td>
+                <td className={styles.approveTableName} >Sjaak Kok</td>
+                <td className={styles.tableDataCentered}>
+                  <FontAwesomeIcon className={`${styles.icon} ${styles.iconCheck}`} icon={faCheck}/>
+                </td>
+                <td className={styles.tableDataCentered}>
+                  <FontAwesomeIcon className={`${styles.icon} ${styles.iconCross}`} icon={faTimes}/>
+                </td>
+              </tr>
+              <tr>
+                <td>Bertassssdklaasasdasdasdasdasdasddasdasdsjd</td>
+                <td className={styles.approveTableName} >Bert de Knaap</td>
+                <td className={styles.tableDataCentered}>
+                  <FontAwesomeIcon className={`${styles.icon} ${styles.iconCheck}`} icon={faCheck} />
+                </td>
+                <td className={styles.tableDataCentered}>
+                  <FontAwesomeIcon className={`${styles.icon} ${styles.iconCross}`} icon={faTimes}/>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+        <h2>Custom points</h2>
+        <p>By default your poule uses ScoreZone's points system that can be found in the <Link href="/rules"><a className={styles.inlineClickable}>rules</a></Link> section. 
+        You can customize points that will be awarded after every match by changing the values below.</p>
+        <div class={styles.pointsTable}>
+          <table>
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="totalscore">Total score correct</label>
+                </th>
+                <td>
+                  <input type="number" name="totalscore" id="totalscore" defaultValue='10'></input>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="drawcorrect">Draw correct</label>
+                </th>
+                <td>
+                  <input type="number" name="drawcorrect" id="drawcorrect" defaultValue='10'></input>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="winnercorrect">Winner correct</label>
+                </th>
+                <td>
+                  <input type="number" name="winnercorrect" id="winnercorrect" defaultValue='10'></input>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="homegoals">Number of home goals correct</label>
+                </th>
+                <td>
+                  <input type="number" name="homegoals" id="homegoals" defaultValue='10'></input>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="awaygoals">Number of away goals correct</label>
+                </th>
+                <td>
+                  <input type="number" name="away goals" id="awaygoals" defaultValue='10'></input>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h2>Delete poule</h2>
+        <p>If you wish to delete the poule you created press the button below.</p>
+        <button className={styles.deleteButton}>Delete poule</button>
       </Layout>
     </>
   )
