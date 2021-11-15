2bc7790de296c3f16c33ef310c6fa07020c85065
Sjaak <sjaakrest@gmail.com>
2021-11-15 13:30:18 +0100

    create logout page

---
+++ pages/logout/index.js
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout'
+
+export default function Logout () {
+  return (
+    <>
+      <Layout>
+        <h1>Logout</h1>
+        <p>You have successfully logged out of your account.</p>
+      </Layout>
+    </>
+  )
+}
