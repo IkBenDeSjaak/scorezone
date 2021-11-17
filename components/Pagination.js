064a1f698cb15ee3fd3826e3e0c7b15b62d9e50e
Sjaak <sjaakrest@gmail.com>
2021-11-17 15:48:37 +0100

    change clickable listen to a href

--- components/Pagination.js
+++ components/Pagination.js
@@ -11,92 +11,46 @@ export default function Pagination ({ page, amountOfPages }) {
     <>
       <ul className={styles.list}>
         {page > 2 && (
-          <li
-            className={styles.item} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: 1 }
-              })
-            }}
-          ><p>{'<<'}</p>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${1}`}><a>{'<<'}</a></Link>
           </li>
         )}
         {page > 1 && (
-          <li
-            className={`${styles.item} ${styles.onePageModifier}`} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: Number(page) - 1 }
-              })
-            }}
-          ><p>{'<'}</p>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${page - 1}`}><a>{'<'}</a></Link>
           </li>
         )}
         {page > 2 && (
-          <li
-            className={styles.item} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: Number(page) - 2 }
-              })
-            }}
-          ><p>{page - 2}</p>
-          </li>
+          <li className={styles.item}>
+          <Link href={`/rankings/${lid}?page=${page - 2}`}><a>{page - 2}</a></Link>
+        </li>
         )}
         {page > 1 && (
-          <li
-            className={styles.item} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: Number(page) - 1 }
-              })
-            }}
-          ><p>{page - 1}</p>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${page - 1}`}><a>{page - 1}</a></Link>
           </li>
         )}
-        <li className={`${styles.item} ${styles.itemSelected}`}><p>{page}</p></li>
+        <li className={`${styles.item} ${styles.itemSelected}`}>
+          <Link href={`/rankings/${lid}?page=${page}`}><a>{page}</a></Link>
+        </li>
         {page <= amountOfPages - 1 && (
-          <li
-            className={styles.item} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: Number(page) + 1 }
-              })
-            }}
-          ><p>{Number(page) + 1}</p>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${page + 1}`}><a>{page + 1}</a></Link>
           </li>
         )}
         {page <= amountOfPages - 2 && (
-          <li
-            className={styles.item} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: Number(page) + 2 }
-              })
-            }}
-          ><p>{Number(page) + 2}</p>
-          </li>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${page + 2}`}><a>{page + 2}</a></Link>
+        </li>
         )}
         {page <= amountOfPages - 1 && (
-          <li
-            className={`${styles.item} ${styles.onePageModifier}`} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: Number(page) + 1 }
-              })
-            }}
-          ><p>{'>'}</p>
-          </li>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${page + 1}`}><a>{'>'}</a></Link>
+         </li>
         )}
         {page <= amountOfPages - 2 && (
-          <li
-            className={styles.item} onClick={() => {
-              router.push({
-                pathname: `/rankings/${lid}`,
-                query: { page: amountOfPages }
-              })
-            }}
-          ><p>{'>>'}</p>
+          <li className={styles.item}>
+            <Link href={`/rankings/${lid}?page=${amountOfPages}`}><a>{'>>'}</a></Link>
           </li>
         )}
       </ul>
