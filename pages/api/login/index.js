65e5f01a7affc1e45e7d9b560880d4787be43e54
Sjaak <sjaakrest@gmail.com>
2021-11-23 16:10:36 +0100

    implement functional login

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,26 +1,52 @@
+import bcrypt from 'bcrypt'
 import { withSessionRoute } from '../../../lib/withSession'
+import { query } from '../../../lib/db'
 
 export default withSessionRoute(handler)
 
 async function handler (req, res) {
   switch (req.method) {
     case 'POST': {
-      // const { username, password } = await req.body
+      const { username, password, rememberme } = await req.body
 
-      const username = 'TESTPERSOON'
-
-      // get user from database then:
-
-      const user = {
-        id: 230,
-        username: username
+      if (!username || !password || rememberme === undefined) {
+        return res.status(400).json({ message: 'Submit a username and password' })
+      } else if (typeof (rememberme) !== 'boolean') {
+        return res.status(400).json({ message: 'Bad request' })
       }
 
-      req.session.user = user
+      const results = await query(
+        `
+        SELECT UserId, Password
+        FROM Users
+        WHERE Username = ?
+        `,
+        username
+      )
 
-      await req.session.save()
+      if (!results[0]) {
+        return res.status(401).json({ message: 'Invalid username or password' })
+      }
 
-      res.status(200).json({ name: `${username} is logged in` })
+      if (results[0]?.Password) {
+        const match = await bcrypt.compare(password, results[0].Password)
+
+        if (match) {
+          const user = {
+            id: results[0].UserId,
+            username: username
+          }
+
+          req.session.user = user
+          await req.session.save()
+
+          return res.status(200).json({ message: 'Valid username and password combination' })
+        } else {
+          return res.status(401).json({ message: 'Invalid username or password' })
+        }
+      } else {
+        return res.status(401).json({ message: 'Invalid username or password' })
+      }
     }
   }
 }
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,19 +1,55 @@
 import styles from './Login.module.css'
 
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Layout from '../../components/Layout'
 
 export default function Login () {
+  const router = useRouter()
+  const [inputFields, setInputFields] = useState({
+    username: '',
+    password: '',
+    rememberme: false
+  })
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const inputsHandler = (e) => {
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    setInputFields({ ...inputFields, [name]: value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputFields)
+    })
+    if (response.status === 200) {
+      router.push('/predictions')
+    } else {
+      const responseJson = await response.json()
+      setErrorMessage(responseJson.message)
+    }
+  }
+
   return (
     <>
       <Layout>
         <h2>Login</h2>
         <p>Login with your username and password.</p>
-        <form className={styles.form} onSubmit={console.log('submit')}>
-          <label className={styles.label} htmlFor='username'>Username<input className={styles.input} id='username' type='text' /></label>
-          <label className={styles.label} htmlFor='password'>Password<input className={styles.input} id='password' type='password' /></label>
+        {errorMessage ? <p className={styles.errorMessage}>{errorMessage}</p> : ''}
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label} htmlFor='username'>Username<input className={styles.input} required name='username' id='username' type='text' value={inputFields.username} onChange={inputsHandler} /></label>
+          <label className={styles.label} htmlFor='password'>Password<input className={styles.input} required name='password' id='password' type='password' value={inputFields.password} onChange={inputsHandler} /></label>
           <label className={styles.label}><input className={`${styles.submitbutton} ${styles.input}`} id='submitbutton' type='submit' value='Login' /></label>
           <label className={styles.labelrememberme}>
-            <input className={styles.inputrememberme} defaultChecked name='rememberme' id='rememberme' type='checkbox' />
+            <input className={styles.inputrememberme} name='rememberme' id='rememberme' type='checkbox' checked={inputFields.rememberme} onChange={inputsHandler} />
             Remember me
           </label>
         </form>
