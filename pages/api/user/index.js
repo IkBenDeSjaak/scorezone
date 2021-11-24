9e77df59c0c3b31e9d6d60a87cfdddfada709e0c
Sjaak <sjaakrest@gmail.com>
2021-11-24 13:12:11 +0100

    remove unnecessary comments

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -6,8 +6,6 @@ async function handler (req, res) {
   switch (req.method) {
     case 'GET':
       if (req.session.user) {
-        // in a real world application you might read the user id from the session and then do a database request
-        // to get more information on the user if needed
         res.json({
           ...req.session.user,
           isLoggedIn: true
