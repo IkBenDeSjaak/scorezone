b6e1f4783814fb10406f960240c3db2ebf6887d5
Sjaak <sjaakrest@gmail.com>
2021-11-24 10:11:08 +0100

    put login and signup in try catch block

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -7,43 +7,47 @@ export default withSessionRoute(handler)
 async function handler (req, res) {
   switch (req.method) {
     case 'POST': {
-      const { username, password } = await req.body
+      try {
+        const { username, password } = await req.body
 
-      if (!username || !password) {
-        return res.status(400).json({ message: 'Submit a username and password' })
-      } 
+        if (!username || !password) {
+          return res.status(400).json({ message: 'Submit a username and password' })
+        }
 
-      const results = await query(
-        `
-        SELECT UserId, Password
-        FROM Users
-        WHERE Username = ?
-        `,
-        username
-      )
+        const results = await query(
+          `
+          SELECT UserId, Password
+          FROM Users
+          WHERE Username = ?
+          `,
+          username
+        )
 
-      if (!results[0]) {
-        return res.status(401).json({ message: 'Invalid username or password' })
-      }
+        if (!results[0]) {
+          return res.status(401).json({ message: 'Invalid username or password' })
+        }
 
-      if (results[0]?.Password) {
-        const match = await bcrypt.compare(password, results[0].Password)
+        if (results[0]?.Password) {
+          const match = await bcrypt.compare(password, results[0].Password)
 
-        if (match) {
-          const user = {
-            id: results[0].UserId,
-            username: username
+          if (match) {
+            const user = {
+              id: results[0].UserId,
+              username: username
+            }
+
+            req.session.user = user
+            await req.session.save()
+
+            return res.status(200).json({ message: 'Valid username and password combination' })
+          } else {
+            return res.status(401).json({ message: 'Invalid username or password' })
           }
-
-          req.session.user = user
-          await req.session.save()
-
-          return res.status(200).json({ message: 'Valid username and password combination' })
         } else {
           return res.status(401).json({ message: 'Invalid username or password' })
         }
-      } else {
-        return res.status(401).json({ message: 'Invalid username or password' })
+      } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' })
       }
     }
   }
--- pages/api/signup/index.js
+++ pages/api/signup/index.js
@@ -4,29 +4,33 @@ import bcrypt from 'bcrypt'
 export default async function handler (req, res) {
   switch (req.method) {
     case 'POST': {
-      const { email, username, password } = req.body
+      try {
+        const { email, username, password } = req.body
 
-      if (!email || !username || !password) {
-        return res.status(400).json({ message: 'Fill in all required fields' })
-      } else if (password.length < 8 || password.length > 50) {
-        return res.status(400).json({ message: 'Password does not meet the required length' })
-      } else if (username.length < 6) {
-        return res.status(400).json({ message: 'Username does not meet the required length' })
-      } else if (email.length > 64) {
-        return res.status(400).json({ message: 'Email is too long' })
+        if (!email || !username || !password) {
+          return res.status(400).json({ message: 'Fill in all required fields' })
+        } else if (password.length < 8 || password.length > 50) {
+          return res.status(400).json({ message: 'Password does not meet the required length' })
+        } else if (username.length < 6) {
+          return res.status(400).json({ message: 'Username does not meet the required length' })
+        } else if (email.length > 64) {
+          return res.status(400).json({ message: 'Email is too long' })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        await query(
+          `
+          INSERT INTO Users (Email, Username, Password, Role, HidePredictions) 
+          VALUES (?, ?, ?, ?, ?)
+          `,
+          [email, username, hashedPassword, 'User', 0]
+        )
+
+        res.status(200).json({ name: `${username} succesfully signed up` })
+      } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' })
       }
-
-      const hashedPassword = await bcrypt.hash(password, 12)
-
-      await query(
-        `
-        INSERT INTO Users (Email, Username, Password, Role, HidePredictions) 
-        VALUES (?, ?, ?, ?, ?)
-        `,
-        [email, username, hashedPassword, 'User', 0]
-      )
-
-      res.status(200).json({ name: `${username} succesfully signed up` })
     }
   }
 }
