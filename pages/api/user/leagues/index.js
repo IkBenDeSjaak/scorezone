3ff12dd3402c016ac533501bf33394ebdb1e36a5
Sjaak <sjaakrest@gmail.com>
2021-11-25 10:01:03 +0100

    create api route to get all user leagues and create a new user league

---
+++ pages/api/user/leagues/index.js
@@ -0,0 +1,57 @@
+import { withSessionRoute } from '../../../../lib/withSession'
+import { query } from '../../../../lib/db'
+
+export default withSessionRoute(handler)
+
+async function handler (req, res) {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const uid = req.session.user.id
+
+        if (!uid) {
+          return res.status(401)
+        }
+
+        const results = await query(
+          `
+          SELECT L.LeagueId 
+          FROM Leagues L 
+          INNER JOIN UserLeagues UL ON L.LeagueId = UL.LeagueId 
+          WHERE UL.UserId = ?
+          `,
+          uid
+        )
+
+        const leagueIds = results.map((league) => league.LeagueId)
+
+        res.status(200).json(leagueIds)
+      } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' })
+      }
+      break
+    case 'POST':
+      try {
+        const uid = req.session.user.id
+        const leagueId = req.body
+
+        if (!uid) {
+          return res.status(401).end()
+        } else if (!leagueId) {
+          return res.status(400).json({ message: 'Missing leagueId in request' })
+        }
+
+        await query(
+          `
+          INSERT INTO UserLeagues (UserId, LeagueId) 
+          VALUES (?, ?)
+          `,
+          [uid, leagueId]
+        )
+
+        res.status(200).end()
+      } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' })
+      }
+  }
+}
