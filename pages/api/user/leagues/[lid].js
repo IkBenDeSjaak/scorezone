31fd520f91c94662c4b96ff70c97cc83b4f11e07
Sjaak <sjaakrest@gmail.com>
2021-11-25 10:01:25 +0100

    create api route to delete a user league

---
+++ pages/api/user/leagues/[lid].js
@@ -0,0 +1,32 @@
+import { withSessionRoute } from '../../../../lib/withSession'
+import { query } from '../../../../lib/db'
+
+export default withSessionRoute(handler)
+
+async function handler (req, res) {
+  switch (req.method) {
+    case 'DELETE':
+      try {
+        const uid = req.session.user.id
+        const leagueId = req.body
+
+        if (!uid) {
+          return res.status(401).end()
+        } else if (!leagueId) {
+          return res.status(400).json({ message: 'Missing leagueId in request' })
+        }
+
+        await query(
+          `
+          DELETE FROM UserLeagues
+          WHERE UserId = ? AND LeagueId = ?
+          `,
+          [uid, leagueId]
+        )
+
+        res.status(200).end()
+      } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' })
+      }
+  }
+}
