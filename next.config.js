01e3b049f4d167dfd2b7020324dc5a25b48bc2c9
Sjaak <sjaakrest@gmail.com>
2021-11-25 15:40:19 +0100

    fix cannot resolve modules errors when querying in getserversideprops

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback.net = false
+      config.resolve.fallback.tls = false
+      config.resolve.fallback.fs = false
+    }
+    return config
+  }
 }
