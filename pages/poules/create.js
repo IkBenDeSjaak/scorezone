002e9d018f91bcf494f166fe186a8c4892914e51
Sjaak <sjaakrest@gmail.com>
2021-11-25 12:48:07 +0100

    make create poule page interactive

--- pages/poules/create.js
+++ pages/poules/create.js
@@ -1,27 +1,76 @@
 import styles from './CreatePoule.module.css'
 
+import { withSessionSsr } from '../../lib/withSession'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import ErrorMessage from '../../components/ErrorMessage'
 import Layout from '../../components/Layout'
 
 export default function CreatePoule () {
+  const router = useRouter()
+  const [leagues, setLeagues] = useState([])
+  const [inputFields, setInputFields] = useState({
+    pouleName: '',
+    leagueId: '',
+    approveParticipants: true
+  })
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(async () => {
+    const leagues = await fetch('/api/leagues', {
+      method: 'GET'
+    }).then((res) => res.json())
+
+    setInputFields({ ...inputFields, leagueId: leagues[0].LeagueId })
+    setLeagues(leagues)
+  }, [])
+
+  const inputsHandler = (e) => {
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+
+    console.log(name)
+    console.log(value)
+    setInputFields({ ...inputFields, [name]: value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await fetch('/api/user/poules', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(inputFields)
+    })
+
+    if (response.status === 201) {
+      router.push('/poules')
+    } else {
+      const responseJson = await response.json()
+      setErrorMessage(responseJson.message)
+    }
+  }
+
   return (
     <>
       <Layout>
         <h1>Create a new poule!</h1>
-        <form className={styles.form} onSubmit={console.log('submit')}>
-          <label className={styles.label} htmlFor='poulename'>Name of the poule
-            <input className={styles.input} id='poulename' type='text' />
+        {errorMessage ? <ErrorMessage message={errorMessage} /> : ''}
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label} htmlFor='pouleName'>Name of the poule
+            <input className={styles.input} id='pouleName' name='pouleName' type='text' onChange={inputsHandler} />
           </label>
           <label className={styles.label} htmlFor='league'>
             League
-            <select className={styles.select} id='league' onChange={console.log('change')}>
-              <option value='grapefruit'>Eredivisie</option>
-              <option value='lime'>Bundesliga</option>
-              <option value='coconut'>Champions League</option>
-              <option value='mango'>Nog een league</option>
+            <select className={styles.select} name='leagueId' id='league' value={inputFields.leagueId} onChange={inputsHandler}>
+              {leagues?.map((league) => (
+                <option key={league.LeagueId.toString()} value={league.LeagueId}>{league.LeagueName}</option>
+              ))}
             </select>
           </label>
-          <label className={styles.labelapprove} htmlFor='approve'>
-            <input className={styles.inputapprove} defaultChecked name='approve' id='approve' type='checkbox' />
+          <label className={styles.labelapprove} htmlFor='approveParticipants'>
+            <input className={styles.inputapprove} name='approveParticipants' id='approveParticipants' type='checkbox' checked={inputFields.approveParticipants} onChange={inputsHandler} />
             I want to be able to approve/disapprove new members
           </label>
           <label className={styles.label}>
@@ -32,3 +81,24 @@ export default function CreatePoule () {
     </>
   )
 }
+
+export const getServerSideProps = withSessionSsr(async function ({
+  req,
+  res
+}) {
+  const user = req.session.user
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+)
