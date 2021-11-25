03a2ad2c3e3dd4db2e1843a91de2f18b13510c35
Sjaak <sjaakrest@gmail.com>
2021-11-25 12:47:18 +0100

    get user poules from db

--- pages/poules/index.js
+++ pages/poules/index.js
@@ -1,40 +1,71 @@
 import styles from './Poules.module.css'
 
+import { withSessionSsr } from '../../lib/withSession'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 
-export default function Poules () {
+export default function Poules ({ user }) {
+  const [poules, setPoules] = useState([])
+
+  useEffect(async () => {
+    if (user) {
+      const poules = await fetch('/api/user/poules', {
+        method: 'GET'
+      }).then(res => res.json())
+
+      setPoules(poules)
+    }
+  }, [])
+
   return (
     <>
       <Layout>
         <div>
           <h1>Poules</h1>
           <h3 className={styles.fontWeightNormal}>Here you can see all the poules you participate in or create a new poule!</h3>
-          <p className={styles.button}><Link href='/poules/create'><a>Create poule</a></Link></p>
-          <div className={styles.poulesContainer}>
-            <div className={`${styles.pouleRow} ${styles.poulesInfo}`}>
-              <p>Poule name</p>
-              <p>League</p>
-              <p>Season</p>
-            </div>
-            <div className={`${styles.pouleRow} ${styles.pouleRowData}`}>
-              <p className={styles.pouleName}><Link href='poules/1234'><a className={styles.pouleText}>Gekke poule</a></Link></p>
-              <p>Eredivisie</p>
-              <p>2020-2021</p>
-            </div>
-            <div className={`${styles.pouleRow} ${styles.pouleRowData}`}>
-              <p className={styles.pouleName}><Link href='poules/12345'><a className={styles.pouleText}>Poule voor collegaasadassdaasdasdasdasdsasdsd's</a></Link></p>
-              <p>Bundesliga</p>
-              <p>2021-2022</p>
-            </div>
-            <div className={`${styles.pouleRow} ${styles.pouleRowData}`}>
-              <p className={styles.pouleName}><Link href='poules/456'><a className={styles.pouleText}>Hele andere pouleeeee</a></Link></p>
-              <p>Champions League</p>
-              <p>2021-2022</p>
-            </div>
-          </div>
+          {user
+            ? (
+              <>
+                <p className={styles.button}><Link href='/poules/create'><a>Create poule</a></Link></p>
+                <div className={styles.poulesContainer}>
+                  <div className={`${styles.pouleRow} ${styles.poulesInfo}`}>
+                    <p>Poule name</p>
+                    <p>League</p>
+                    <p>Season</p>
+                  </div>
+                  {poules?.map((poule) => (
+                    <div key={poule.PouleId.toString()} className={`${styles.pouleRow} ${styles.pouleRowData}`}>
+                      <p className={styles.pouleName}><Link href={`poules/${poule.PouleId}`}><a className={styles.pouleText}>{poule.PouleName}</a></Link></p>
+                      <p>{poule.LeagueName}</p>
+                      <p>{poule.SeasonName}</p>
+                    </div>
+                  ))}
+                </div>
+              </>)
+            : (<p>You can join and create poules when you are logged in.</p>)}
         </div>
       </Layout>
     </>
   )
 }
+
+export const getServerSideProps = withSessionSsr(async function ({
+  req,
+  res
+}) {
+  const user = req.session.user
+
+  if (user) {
+    return {
+      props: {
+        user: user.id
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+)
