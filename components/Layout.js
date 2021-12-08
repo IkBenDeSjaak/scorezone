bfa818d4634f7795669f1cc283fff2c3524c4f07
Sjaak <sjaakrest@gmail.com>
2021-12-08 11:58:04 +0100

    add scorezone custom favicon

--- components/Layout.js
+++ components/Layout.js
@@ -8,9 +8,16 @@ export default function Layout ({ children }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
         <title>ScoreZone</title>
-        <meta name='description' content='ScoreZone maakt het mogelijk voetbalwedstrijden te voorspellen.' />
+        <meta name='description' content='ScoreZone makes it possible to predict football matches.' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#2a8378' />
+        <meta name='msapplication-TileColor' content='#00aba9' />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       <Header />
       <main className={styles.main}>
